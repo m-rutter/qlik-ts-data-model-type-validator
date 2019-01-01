57c67d759c9192087a4a3ca0f3e956aa9e6e48df
Michael Rutter <michael.john.rutter@gmail.com>
2019-01-01 15:42:49 +0000

    untested capabilities api impl

--- src/withCapabilities.ts
+++ src/withCapabilities.ts
@@ -8,5 +8,39 @@ type QlikVariableIdents = string[];
 export async function withCapabilities(
     app: AppAPI.IApp,
 ): Promise<[QlikFieldIdents, QlikVariableIdents]> {
-    throw new Error('Capabilities option not yet implemented');
+    const [fieldListObject, variableListObj]: [
+        EngineAPI.IFieldListObject,
+        EngineAPI.IVariableListObject
+    ] = await Promise.all([
+        app.createGenericObject({
+            qFieldListDef: {},
+        }),
+        app.createGenericObject({
+            qVariableListDef: {
+                qData: {
+                    tags: '/tags',
+                },
+                qShowConfig: true,
+                qShowReserved: true,
+                qType: 'VariableList',
+            },
+        }),
+    ]);
+
+    const [fieldLayout, variableLayout]: [any, any] = await Promise.all([
+        fieldListObject.getLayout(),
+        variableListObj.getLayout(),
+    ]);
+
+    app.destroySessionObject(variableListObj.id);
+    app.destroySessionObject(fieldListObject.id);
+
+    const fieldIdentifiers: string[] = fieldLayout.qFieldList.qItems.map(
+        (f: { qName: string }) => f.qName,
+    );
+    const variableIdentifiers: string[] = variableLayout.qVariableList.qItems.map(
+        (v: { qName: string }) => v.qName as string,
+    );
+
+    return [fieldIdentifiers, variableIdentifiers];
 }
